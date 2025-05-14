04dcd8acd5bae102743a82b4c5305101fb4f283f
Алена Квашнина <alena.kvashnina05@mail.ru>
2025-05-14 18:43:15 +0700

    Книги список

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 450,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 780,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 610,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 2021,
+        price: 500,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Фрэнк Герберт',
+        name: 'Дюна',
+        publicationYear: 2020,
+        price: 820,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Стивен Кинг',
+        name: 'Оно',
+        publicationYear: 2018,
+        price: 670,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 2023,
+        price: 490,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Артур Конан Дойл',
+        name: 'Собрание рассказов о Шерлоке Холмсе',
+        publicationYear: 2017,
+        price: 530,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Габриэль Гарсиа Маркес',
+        name: 'Сто лет одиночества',
+        publicationYear: 2016,
+        price: 710,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джон Толкин',
+        name: 'Властелин колец',
+        publicationYear: 2015,
+        price: 990,
+        bestseller: true,
+        inStock: false
+    }
+];
